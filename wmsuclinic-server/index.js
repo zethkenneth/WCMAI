3266542b940bb58635d819cc72e0d25cd362598e
LvnReyS <elvinreysa@gmail.com>
2020-04-16 18:06:01 +0800

    for testing heroku

--- wmsuclinic-server/index.js
+++ wmsuclinic-server/index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-var path = require('path')
-var serveStatic = require('serve-static')
+const path = require('path');
+var serveStatic = require('serve-static');
 //middleware
 
 app.use(express.json()); //req.body
 app.use(cors());
-app.use(express.static("public"));
-app.use(serveStatic(path.join(__dirname, 'dist')))
+
+
 //routes
 
 //register and login routes
@@ -21,15 +21,21 @@ app.use( '/auth' , require( './routes/jwtAuth' ) );
 app.use("/dashboard", require("./routes/dashboard"));
 //app.use("/dashboard", require("./routes/dashboard"));
 
-//settings
+//settings 
 app.use("/settings", require("./routes/settings"));
 //patient routes
 app.use("/transaction", require("./routes/transaction"));
 
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-});
+//server static folder if in prod.
+if(process.env.NODE_ENV === 'production') {
+//set static folder
+ app.use(express.static('wmsuclinic-client/build'));
+ 
+ app.get('*', (req ,res) => {
+    res.sendFile(path.resolve(_dirname, 'wmsuclinic-client', 'build', 'index.html'));
+ })
+}
 
 app.listen(port, () =>{
     console.log('server started at port : ' + port);
