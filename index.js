e475d276b24fa25a87b13750bac9197c4d2af57b
LvnReyS <elvinreysa@gmail.com>
2020-04-19 10:51:16 +0800

    testing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = server.listen(process.env.PORT || 5000);
+const port = process.env.PORT || 5000;
 const path = require('path');
 const dev = app.get('env') !== 'production';
 const morgan = require('morgan');
