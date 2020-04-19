942b024f756092b169ce056068c719a15eab7663
LvnReyS <elvinreysa@gmail.com>
2020-04-19 11:36:52 +0800

    testing

--- db.js
+++ db.js
@@ -1,10 +1,20 @@
 const Pool = require("pg").Pool
 
 const pool = new Pool({
-    user: "kgkwnyxdoyxyjg",
-    password: "f6ca20c627ced5ef036d9e287a442f0744b6c82323b407a7e72620023324c810",
-    host: "ec2-23-20-129-146.compute-1.amazonaws.com",
-    database: "d6400mnf6uapl9"
+    user: "postgres",
+    password: "root",
+    host: "localhost",
+    database: "wmsuclinic"
+
+
+    //databse on heroku
+    // user: "kgkwnyxdoyxyjg",
+    // password: "f6ca20c627ced5ef036d9e287a442f0744b6c82323b407a7e72620023324c810",
+    // host: "ec2-23-20-129-146.compute-1.amazonaws.com",
+    // database: "d6400mnf6uapl9"
+
+
+
 });
 
 module.exports = pool;
